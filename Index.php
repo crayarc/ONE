a2cb5a4f8598bf7b368a07a2ff8102c5a44c4cc9
Mark Aguilar <aguilarmark19@gmail.com>
2017-07-24 16:49:20 -0500

    skeleton

--- Index.php
+++ Index.php
@@ -1,6 +1,60 @@
+<!DOCTYPE html>
 <html>
-<head></head>
+<head>
+	<link rel='stylesheet' href='main.css'/>
+	<script src='script.js'></script>
+</head>
 <body>
-<h1>Home Page</h1>
+
+<div>
+<h1><b>Resume</b></h1>
+<h2>Mark A. Aguilar</h2>
+<p><u>aguilarmark63@yahoo.com</ul></p>
+<p>(832) 229-0061</p>
+<p>markaguilar.cf</p>
+</div>
+
+<div class="education">
+    <h2>Education</h2>
+    <p class="left">University of Houston College of Technology, Houston, Texas</p>
+    <p class="right">Fall 2016</p>
+    <p><b>Bachelor of Science in Computer Information Systems</b></p>
+    <p>Lone Star College, Houston, Texas</p>
+    <p><b>Associates of Science</b></p>
+</div>
+
+<div class="employment">
+    <h2>Employment History</h2>
+    <p>Guest Services Representative, <b>Houston Astros</b>,  Houston TX</p>
+    <ul>
+        <li>Scan game tickets insuring that the proper ticket is being used to gain entry</li>
+        <li>Assist fans in locating their ticketed seats</li>
+        <li>Help by preparing your assigned section for patrons</li>
+        <li>Monitor assigned area for issues</li>
+        <li>Being alert to potential hazards in the ballpark such as reporting incidents</li>
+    </ul>
+
+    <p>Sales Associate/Key Holder, <b>RadioShack</b>, Houston, TX</p>
+    <ul>
+        <li>In control of opening/closing the store and taking the deposit.</li>
+        <li>Using communication skills to help customers with what they are looking for</li>
+        <li>Working together with a team in order to achieve objectives.</li>
+        <li>Sell approximately $9,000 worth of merchandise and 300 tickets monthly.</li>
+    </ul>
+
+    <p>Maintenance Associate, <b>Huber Tennis Ranch</b>, Cypress, TX</p>
+    <ul>
+        <li>Strung 10 tennis rackets per week for members</li>
+        <li>Managed $1,000 worth of sports inventory</li>
+    </ul>
+</div>
+
+<div class="skills">
+    <h2>Skills</h2>
+    <ul>
+        <li>Languages: C++, CSS, HTML, JavaScript</li>
+    </ul>
+</div>
+
 </body>
 </html>
